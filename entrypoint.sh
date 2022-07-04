78abccf7e771ff1aa5670a5e1946eccf5d5a2d41
John Valai <git@jvk.to>
2022-07-05 00:13:37 +0100

    refactor: patched code to pass SC tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,22 +28,21 @@ if [ -z "$GS_INPUT_FILE" ] || [ ! -f "$GS_INPUT_FILE" ]; then
   error "Input file <${GS_INPUT_FILE}> is empty or doesn't exist."
 fi
 
-if [ -f "$GS_OUTPUT_FILE" ] && {$OVERWRITE}; then
+[ -f "$GS_OUTPUT_FILE" ] && [ -z "$OVERWRITE" ] && \
   error "Output file <${GS_OUTPUT_FILE}> already exists — please use another name."
-fi
 
 [ -z "${GITHUB_TOKEN}" ] && {
-    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
-    exit 1;
-};
+    echo "Missing input \"github_token: \${{ secrets.GITHUB_TOKEN }}\"."
+    exit 1
+}
 
-cmd_gs='gs ${GS_DEFAULT_PARAMS} -sOutputFile=${GS_OUTPUT_FILE} ${GS_OPTIONAL_PARAMS} ${GS_INPUT_FILE}'
+cmd_gs="gs ${GS_DEFAULT_PARAMS} -sOutputFile=${GS_OUTPUT_FILE} ${GS_OPTIONAL_PARAMS} ${GS_INPUT_FILE}"
 
-cmd_set_safe_dir='git config --global --add safe.directory /github/workspace'
+cmd_set_safe_dir="git config --global --add safe.directory /github/workspace"
 
 # Copy the name and email from the last commit
-cmd_set_email='git config --local user.email "$(git log --format='"'"'%ae'"'"' HEAD^!)"'
-cmd_set_name='git config --local user.name "$(git log --format='"'"'%an'"'"' HEAD^!)"'
+cmd_set_email="git config --local user.email \"\$(git log --format='%ae' HEAD^\!)\""
+cmd_set_name="git config --local user.name \"\$(git log --format='%an' HEAD^\!)\""
 
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GH_REPO}.git"
 
@@ -58,5 +57,3 @@ eval "$cmd_gs" && \
 eval "$cmd_stage" && \
 eval "$cmd_commit" && \
 eval "$cmd_push"
-
-
